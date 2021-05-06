de73df3cdddb202a89a9d621dbcb2588e7201dd4
Fora127 <mariasmol@inbox.ru>
2021-05-06 21:40:11 +0300

    added modal and tooltip. New version of calendar

---
+++ 05-lection3/02-modal/index.js
@@ -0,0 +1,21 @@
+(function() {
+    const close = document.getElementById('modal-close');
+    const modal = document.getElementById('modal');
+    const modalOpen = document.getElementById('modal-open');
+
+    if (!close || !modalOpen || !modal) {
+        return;
+    }
+
+    modalOpen.addEventListener('click', () => {
+        console.log('buttonPressed');
+        modal.style.display = 'block';
+        document.body.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', () => {
+        modal.style.display = 'none';
+        document.body.overflow = 'initial';
+    });
+})();
+
